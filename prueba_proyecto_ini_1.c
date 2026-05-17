25b1b45a474de97eeea209de2fe4f4033c14bc39
VedzabethCortez <vedzacortezm@gmail.com>
2026-05-17 11:26:50 -0400

    Menu2idiomas
    Co-Authored-By: jimmeliscolina-arch <276646315+jimmeliscolina-arch@users.noreply.github.com>
    Co-Authored-By: Paolaescobar04 <276718941+Paolaescobar04@users.noreply.github.com>

--- prueba_proyecto_ini_1.c
+++ prueba_proyecto_ini_1.c
@@ -7,14 +7,12 @@ IDEA: Juego con 12 personajes y 4 grupos (3 por grupo)  5 VS5 .
 3. Llaves {}: Abrirlas en una línea nueva para separar mejor los bloques.
 4. Indentación: Respetar el sangrado reglamentario para mantener el orden.
 5. Idioma: El profesor mencionó que la creación de variables debía ser en inglés... aunque mas adelante las traducire*/
-/* Librerias usadas*/
+// Librerias usadas
 #include <stdio.h>   // necesaria para imprimir y obtener datos del usuario
 #include <stdbool.h> // para poder usar valores verdaderos o falsos
 #include <stdlib.h>  // funciones como rand y srand para numeros "aleatorios"
 #include <time.h>    // necesaria para que la funcion srand tome el reloj interno del computador como "semilla"
 #include <math.h> // Necesaria para operaciones matematicas un poco mas avanzadas
-
-
 // ===================ESTRUCTURAS GLOBLALES=============
 //<<<<<grupos de cartas  hechos por PAola>>
 typedef struct 
@@ -29,35 +27,27 @@ typedef struct
     int Energy; // Energía del Personaje 
 } Character;
 // ==================VARIABLES GLOBALEs====================
-
 Character Roster[12]; // Todos los Personajes que son parte del Juego.
 int Lenguaje=0; // 0 es español y 1 ingles
 bool JuegoEncendido =true;//por ahora solo la que conrtola el apagado del juego
-
-
-
 //===============ARREGLOS GLOBALES===============
-
-
-
 /* ==============INDICE DE PROTOTIPOS(FUNCIONES) ===========
 se iran mencionando aca para que el programa sepa que en
 algun momento conseguira una funcion con ese nombre*/
 char ShowPrincipalMenu();
-void Principaloop();
+void PrincipalLoop();
 void MenuJump();
 void CreateRoster();//crea a los personajes (Paola)
 void ShowRoster();//muestra a los personajes en catalogo
 /*añadir 2 funciones int para SeleccionDePersonaje
  y recordar hacer una funcion aleatrorio entera  para el dado*/
 void SelectLanguage();// español opcion 1, ingles opcion 2
-
 //===========INT MAIN donde inicializamos algunas funciones ========
 int main()
 {   
-	CreateRoster();
 	SelectLanguage();
-	Principaloop();
+	CreateRoster();
+	PrincipalLoop();
 	return 0;
 }
 /*===================================================================================
@@ -66,131 +56,229 @@ int main()
 void MenuJump()
 {
 	printf("\n>>>>>>>>>>>>>>>>>>>>O<<<<<<<<<<<<<<<<<<<<\n");
-	printf("=========================================\n");
 	printf("\n");
-	printf("=========================================\n");
 	printf("\n>>>>>>>>>>>>>>>>>>>>O<<<<<<<<<<<<<<<<<<<<\n");
 }
 /* aca se elije el idioma del juego*/
 void SelectLanguage()
 {
-	int Option;
-
-	printf("Selecciona un Idioma //select a lenguaje \n");
+	char Option;
+	printf("Selecciona un Idioma // Select a language \n");
 	printf("=========================================\n");
 	printf("1- ESPANIOL       //      1-SPANISH   \n");
-	printf("2-   INGLES       //      2-ENGLISH   \n");
+	printf("2- INGLES         //      2-ENGLISH   \n");
 	printf("Su Seleccion      // Your selection: -> ");
-	scanf("%d", &Option);
-	if(Option==2)
+	scanf(" %c", &Option); 
+	if(Option == '2') 
 	{
-		Lenguaje=1;
+		Lenguaje = 1; 
 		printf("=========================================\n");
 		printf("Starting the game in **ENGLISH**\n");
 	}
-	else
+	else if(Option == '1')
 	{
-		Lenguaje=0;
+		Lenguaje = 0; 
 		printf("=========================================\n");
-		printf("inicializando el juego en **ESPANIOL**\n");
+		printf("Inicializando el juego en **ESPANIOL**\n");
 	}
+	else 
+	{
+		// Si escriben otra cosa, forzamos español
+		Lenguaje = 0; 
+		printf("=========================================\n");
+		printf("Opcion invalida. Inicializando por defecto en **ESPANIOL**\n");
+	}
+	
 	MenuJump();
 }
-//funcion encargada del menu principal//
+/*funcion encargada del menu principa, use char ya que un numero es igual un caracter y asi nos
+ salvamos de un bucle infinito si sin querer escribian una letra u otro simbolo salvamos de u
+ n bucle infinito si sin querer escribian una letra u otro simbolo*/
+ 	
 char ShowPrincipalMenu()
 {
 	char SeleccionUsuario;
-	printf ("\n ====Ecos de Traicion y Horror en C===\n\n \nPOR FAVOR SELECCIONE UNA OPCION\n");
-	printf ("\n 1-- Ver lista de personajes \n \n 2-- Formar Equipos \n");
-	printf ("\n 3-- Ejecutar Batalla \n \n 4-- Mostar Resultados\n");
-	printf ("\n 5-- SALIR DEL JUEGO!\n \n SU SELECCION   ");
+	if (Lenguaje == 0) // Si es 0, mostramos todo en ESPANIOL
+	{
+		printf ("\n ==== Ecos de Traicion y Horror en C ====\n\n");
+		printf (" POR FAVOR SELECCIONE UNA OPCION\n");
+		printf ("\n 1-- Ver lista de personajes \n");
+		printf ("\n 2-- Formar Equipos \n");
+		printf ("\n 3-- Iniciar Batalla \n");
+		printf ("\n 4-- Mostrar Resultados \n");
+		printf ("\n 5-- SALIR DEL JUEGO!\n");
+		printf ("\n SU SELECCION   ");
+	}
+	else // Si es 1 (o cualquier otra cosa), mostramos todo en INGLES
+	{
+		printf ("\n ==== Echoes of Treason and Horror in C ====\n\n");
+		printf (" PLEASE SELECT AN OPTION\n");
+		printf ("\n 1-- View character list \n");
+		printf ("\n 2-- Form Teams \n");
+		printf ("\n 3-- Start Battle \n");
+		printf ("\n 4-- Show Results \n");
+		printf ("\n 5-- EXIT GAME!\n");
+		printf ("\n YOUR SELECTION   ");
+	}
 	scanf(" %c", &SeleccionUsuario);
 	MenuJump();
 	return SeleccionUsuario;
 }
-/* es el bucle principal donde se inicia el juego*/
-void Principaloop()
+// es el bucle principal donde se inicia el juego
+void PrincipalLoop()
 {
-    char OpcionDeInicio;
-
-    while (JuegoEncendido == true)
-    {
-        OpcionDeInicio = ShowPrincipalMenu();
-        switch (OpcionDeInicio) 
-        {
-            case '1':
-            	printf("\n   LISTA DE PERSONAJES\n");
-            	ShowRoster();
-            	// llamamos al catalogo
-            break;
-            
-            case '2':
-                printf("\n CREACION DE EQUIPOS\n");
-                // aca mostrar y hacer el ciuclo de seleccion de los personajes y al final cartas de ambiente 
-            break;
-                
-            case '3':
-                printf("\nINICIAR ENFRENTAMIENTO\n");
-            break;
-                
-            case '4':
-                printf("\nREGISTRO  DE  VENCEDORES  EN  IRONHEVEN\n");
-            break;
-                
-            case '5':
-                printf("\n*** =SALIENDO DEL JUEGO GRACIAS POR JUGAR! ***\n");
-                JuegoEncendido = false;
-            break;
-                
-            default:
-                printf("\n==Opcion no valida! Intente de nuevo Por favor===\n");
-            break;
-            
-        }	MenuJump();
-    }	
-    
+	char OpcionDeInicio;
+	while (JuegoEncendido == true)
+	{
+		OpcionDeInicio = ShowPrincipalMenu();
+		switch (OpcionDeInicio) 
+		{	
+			case '1':
+				if (Lenguaje == 0) 
+				{ 
+					printf("\n   LISTA DE PERSONAJES\n"); 
+				}
+				else 
+				{ 
+					printf("\n   CHARACTER LIST\n"); 
+				}
+				ShowRoster(); // Llamamos al catalogo
+			break;
+			case '2':
+				if (Lenguaje == 0)
+				{
+				  printf("\n CREACION DE EQUIPOS\n"); 
+				}
+				else
+				{
+				  printf("\n TEAM CREATION\n");
+				}
+			break;
+			case '3':
+				if (Lenguaje == 0)
+				{
+				  printf("\n INICIAR BATALLA\n"); 
+				}
+				else
+				{
+				  printf("\n START BATTLE \n");
+				}
+			break;
+			case '4':
+				if (Lenguaje == 0)
+				{
+				  printf("\nREGISTRO DE VENCEDORES EN IRONHAVEN\n"); 
+				}
+				else
+				{
+				  printf("\nRECORD OF VICTORS IN IRONHAVEN\n");
+				}	
+			break;
+			case '5':
+				if (Lenguaje == 0)
+				{
+				  printf("\n*** =SALIENDO DEL JUEGO GRACIAS POR JUGAR! ***\n"); 
+				}
+				else
+				{
+				  printf("\n*** EXITING GAME THANKS FOR PLAYING! ***\n");
+				}
+				JuegoEncendido = false;				
+			break;
+			default:
+				if (Lenguaje == 0)
+				{
+				  printf("\n==Opcion no valida! Intente de nuevo Por favor===\n"); 
+				}
+				else
+				{
+				  printf("\n==Invalid option! Please try again===\n");
+				}			
+			break;
+		}	
+		MenuJump();
+	}	
 }
 /*===================================================================================
 =====================================================================================
-======================SECCION DE PERSONAJES y  CAALOGO===============================*/
-void CreateRoster()
- {
-    /* Aquí se crean los Personajes junto a sus estadísticas; aquí puedes editar tanto sus 
-    Estadísticas cómo su nombre, y cualquier otro Valor único de cada personaje. */
-    
-    // { ID,"Grupo","Tipo","Nombre", Vida, Defensa, Ataque, Energía}
-    Character creating[12] = 
-	{
-        // Nobles de Ironhaven
-        { 1,"Nobles de Ironheven","Guerrero","Alaric", 150, 150, 150, 150}, 
-        { 2,"Nobles de Ironheven","Distancia","Elara", 100, 50, 200, 250}, 
-        { 3,"Nobles de Ironheven","Tanque","Gareth, el Leal", 250, 200, 100, 50}, 
-        // Los Cristalizados
-        {4,"Los Cristalizados","Tanque","Espectro de Obsidiana", 250, 200, 100, 50}, 
-        {5,"Los Cristalizados","Guerrero" ,"Jaspe, el Baron Rojo", 150, 150, 150, 150}, 
-        {6,"Los Cristalizados","Distancia","Selene, Demonio de Plata", 100, 50, 200, 250}, 
-        // Cazadores Hostiles
-        {7,"Cazadores Hostiles","Tanque","Gehrman, el Verdugo", 250, 200, 100, 50}, 
-        {8,"Cazadores Hostiles","Distancia" ,"Kira, Asesino de Monstruos", 100, 50, 200, 250}, 
-        {9,"Cazadores Hostiles","Guerrero","Orion, Cazador Celestial", 150, 150, 150, 150}, 
-        // La Bruma Escarlata
-        {10,"La Bruma Escarlata","Guerrero","El Arcano", 300, 50, 0, 250}, 
-        {11,"La Bruma Escarlata","Tanque","Rey Cedric", 250, 200, 100, 50}, 
-        {12,"La Bruma Escarlata","Distancia","Principe Arthur", 150, 150, 150, 150}
-    };
-    for (int i = 0; i < 12; i++) 
-        Roster[i] = creating[i];
-}
-
-void ShowRoster() 
+=============CREACION,SECCION DE PERSONAJES y CATALOGO===============================*/
+void CreateRoster() // paola
 {
-    printf("****       CATALOGO DE PERSONAJES DE IRONHAVEN        ****\n");
-    for (int i = 0; i < 12; i++) 
+    // Si el lenguaje es ESPANIOL (0)
+    if (Lenguaje == 0) 
     {
-	printf("\n ====================================================================");
-        printf("\nID:    %d   |Grupo: %s\n", Roster[i].CharacterID,Roster[i].Group );
-        printf(" Nombre: %s   |Tipo: %s\n",Roster[i].Name , Roster[i].Type);
-        printf("       ESTADISTICAS\n HP: %.2f  |DEF: %.2f \n ATK: %.2f | PODER: %d\n",Roster[i].Hp, Roster[i].Def, Roster[i].Atk, Roster[i].Energy);       
+        Character creatingES[12] = 
+        {
+            // Nobles de Ironhaven
+            { 1,"Nobles de Ironhaven","Guerrero","Alaric", 150, 150, 150, 150}, 
+            { 2,"Nobles de Ironhaven","Distancia","Elara", 100, 50, 200, 250}, 
+            { 3,"Nobles de Ironhaven","Tanque","Gareth, el Leal", 250, 200, 100, 50}, 
+            // Los Cristalizados
+            { 4,"Los Cristalizados","Tanque","Espectro de Obsidiana", 250, 200, 100, 50}, 
+            { 5,"Los Cristalizados","Guerrero" ,"Jaspe, el Baron Rojo", 150, 150, 150, 150}, 
+            { 6,"Los Cristalizados","Distancia","Selene, Demonio de Plata", 100, 50, 200, 250}, 
+            // Cazadores Hostiles
+            { 7,"Cazadores Hostiles","Tanque","Gehrman, el Verdugo", 250, 200, 100, 50}, 
+            { 8,"Cazadores Hostiles","Distancia" ,"Kira, Asesino de Monstruos", 100, 50, 200, 250}, 
+            { 9,"Cazadores Hostiles","Guerrero","Orion, Cazador Celestial", 150, 150, 150, 150}, 
+            // La Bruma Escarlata
+            {10,"La Bruma Escarlata","Guerrero","El Arcano", 300, 50, 0, 250}, 
+            {11,"La Bruma Escarlata","Tanque","Rey Cedric", 250, 200, 100, 50}, 
+            {12,"La Bruma Escarlata","Distancia","Principe Arthur", 150, 150, 150, 150}
+        };
+        for (int i = 0; i < 12; i++) Roster[i] = creatingES[i];
     }
-
+    // Si el lenguaje es INGLES
+    else 
+    {
+        Character creatingEN[12] = 
+        {
+            // Ironhaven Nobles
+            { 1,"Ironhaven Nobles","Warrior","Alaric", 150, 150, 150, 150}, 
+            { 2,"Ironhaven Nobles","Ranged","Elara", 100, 50, 200, 250}, 
+            { 3,"Ironhaven Nobles","Tank","Gareth, the Loyal", 250, 200, 100, 50}, 
+            // The Crystallized
+            { 4,"The Crystallized","Tank","Obsidian Wraith", 250, 200, 100, 50}, 
+            { 5,"The Crystallized","Warrior" ,"Jasper, the Red Baron", 150, 150, 150, 150}, 
+            { 6,"The Crystallized","Ranged","Selene, Silver Demon", 100, 50, 200, 250}, 
+            // Hostile Hunters
+            { 7,"Hostile Hunters","Tank","Gehrman, the Executioner", 250, 200, 100, 50}, 
+            { 8,"Hostile Hunters","Ranged" ,"Kira, Monster Hunter", 100, 50, 200, 250}, 
+            { 9,"Hostile Hunters","Warrior","Orion, Celestial Hunter", 150, 150, 150, 150}, 
+            // The Scarlet Mist
+            {10,"The Scarlet Mist","Warrior","The Arcane", 300, 50, 0, 250}, 
+            {11,"The Scarlet Mist","Tank","King Cedric", 250, 200, 100, 50}, 
+            {12,"The Scarlet Mist","Ranged","Prince Arthur", 150, 150, 150, 150}
+        };
+        for (int i = 0; i < 12; i++) Roster[i] = creatingEN[i];
+    }
 }
+
+void ShowRoster() //CATALOGO DE PERSONAJES
+{
+	if (Lenguaje == 0) 
+	{
+	 	printf("****       CATALOGO DE PERSONAJES DE IRONHAVEN        ****\n");
+    	for (int i = 0; i < 12; i++) 
+    	{
+			printf("\n ====================================================================");
+        	printf("\nID:    %d   |Grupo: %s\n", Roster[i].CharacterID,Roster[i].Group );
+        	printf(" Nombre: %s   |Tipo: %s\n",Roster[i].Name , Roster[i].Type);
+        	printf("       ESTADISTICAS\n HP: %.2f  |DEF: %.2f \n ATK: %.2f | ENERGIA: %d\n",Roster[i].Hp, Roster[i].Def, Roster[i].Atk, Roster[i].Energy);       
+    	}
+	}
+	else 
+	{
+		printf("****       IRONHAVEN CHARACTER CATALOG       ****\n");
+    	for (int i = 0; i < 12; i++) 
+    	{
+			printf("\n ====================================================================");
+        	printf("\nID:    %d   |Group:: %s\n", Roster[i].CharacterID,Roster[i].Group );
+        	printf(" Name: %s   |Type: %s\n",Roster[i].Name , Roster[i].Type);
+        	printf("       STATISTICS\n HP: %.2f  |DEF: %.2f \n ATK: %.2f | ENERGY: %d\n",Roster[i].Hp, Roster[i].Def, Roster[i].Atk, Roster[i].Energy);       
+    	}
+	}
+    	
+}
+
+
